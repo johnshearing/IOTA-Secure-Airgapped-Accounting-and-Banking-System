b663116fe5843d5d76645ec9513f93002d335ee2
John Shearing <johnshearing@gmail.com>
2020-03-06 08:16:27 -0500

    Built logic for enforcing schema on nested docs and posting

--- build.js
+++ build.js
@@ -6,9 +6,9 @@
 const meta = require('./lib/meta');
 
 
-// Build webpages for working with tables.
+// Build webpages, JavaScript, and server side logic for working with tables.
 // The parameter is metadataId in metadata.json.
-meta.build.addWebPage(1065);
-meta.build.editWebPage(1065);
-meta.build.listWebPage(1065);  
+// meta.build.addWebPage(1065);
+// meta.build.editWebPage(1065);
+// meta.build.listWebPage(1065);  
 meta.build.serverSideLogic(1065); 
