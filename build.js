9bb6b1a037c6c9841b2029a85a66d967f671c94e
John Shearing <johnshearing@gmail.com>
2020-03-30 01:33:58 -0400

    Logic for moving nested data within DOM now in generator

--- build.js
+++ build.js
@@ -8,7 +8,7 @@ const meta = require('./lib/meta');
 
 // Build webpages, JavaScript, and server side logic for working with tables.
 // The parameter is metadataId in metadata.json.
-// meta.build.addWebPage(1006);
+meta.build.addWebPage(1006);
 // meta.build.editWebPage(1006);
 // meta.build.listWebPage(1006);  
 // meta.build.serverSideLogic(1006);
